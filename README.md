3e5565b553df75aa6612d0add250b8f58832e832
Eugeny Burnashov <shamcode@gmail.com>
2019-10-07 21:11:20 +0700

    @inject macro

---
+++ README.md
@@ -0,0 +1,14 @@
+# sham-ui-macro
+[![Build Status](https://travis-ci.org/shamcode/sham-ui-macro.svg?branch=master)](https://travis-ci.org/shamcode/sham-ui-macro)
+[![npm version](https://badge.fury.io/js/sham-ui-macro.svg)](https://badge.fury.io/js/sham-ui-macro)
+[![MIT Licence](https://badges.frapsoft.com/os/mit/mit.svg?v=103)](https://opensource.org/licenses/mit-license.php)
+
+Babel macros for `sham-ui`.
+
+### Install
+Install from yarn:
+`yadn add sham-ui --dev`
+
+And add `babel-macros` plugin to your babel config. 
+
+### API
---
+++ __tests__/inject.js
@@ -0,0 +1,51 @@
+/* eslint-env node */
+
+const path = require( 'path' );
+const pluginTester = require( 'babel-plugin-tester' );
+const plugin = require( 'babel-plugin-macros' );
+
+const projectRoot = path.join( __dirname, '../../' );
+
+expect.addSnapshotSerializer( {
+    print( val ) {
+        return val.split( projectRoot ).join( '<PROJECT_ROOT>/' );
+    },
+    test( val ) {
+        return typeof val === 'string';
+    }
+} );
+
+pluginTester( {
+    plugin,
+    snapshot: true,
+    babelOptions: {
+        filename: __filename,
+        parserOpts: {
+            plugins: [
+                'decorators',
+                'classProperties'
+            ]
+        }
+    },
+    tests: [ {
+        title: '@inject store',
+        code: `
+            import inject from '../inject.macro'
+
+            class FooComponent extends Template {
+                @inject store;
+            }
+        `
+    }, {
+        title: '@inject(\'sham-ui:store\') store',
+        code: `
+            import inject from '../inject.macro'
+
+            class FooComponent extends Template {
+                @inject(\'sham-ui:store\') store
+            }
+        `
+    } ]
+} );
+
+
