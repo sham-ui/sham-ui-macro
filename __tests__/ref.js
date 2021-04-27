6843c91faa1d35b3dd29730b958ef5f9b243eef9
Eugeny Burnashov <shamcode@gmail.com>
2021-04-27 19:09:32 +0700

    Remove inject, mergeWithTemplate, add ref for new optimization sham-ui

---
+++ __tests__/ref.js
@@ -0,0 +1,64 @@
+/* eslint-env node */
+
+const path = require( 'path' );
+const pluginTester = require( 'babel-plugin-tester' );
+const plugin = require( 'babel-plugin-macros' );
+
+const projectRoot = path.join( __dirname, '../../' );
+
+expect.addSnapshotSerializer( {
+    print( val ) {
+        return val.split( projectRoot ).join( '<PROJECT_ROOT>/' );
+    },
+    test( val ) {
+        return typeof val === 'string';
+    }
+} );
+
+pluginTester( {
+    plugin,
+    snapshot: true,
+    babelOptions: {
+        filename: __filename,
+        parserOpts: {
+            plugins: [
+                'decorators',
+                'classProperties'
+            ]
+        }
+    },
+    tests: [ {
+        title: 'without args',
+        code: `
+            import { ref } from '../babel.macro';
+
+            const foo = ref();
+        `,
+        snapshot: false
+    }, {
+        title: 'with args',
+        code: `
+            import { ref } from '../babel.macro';
+
+            const foo = ref( 'bar' ) ;
+        `
+    }, {
+        title: 'as key',
+        code: `
+            import { ref } from '../babel.macro';
+
+            const state = { [ ref( 'foo' ) ]: true }; 
+        `
+    }, {
+        title: 'with inject',
+        code: `
+            import { inject, ref } from '../babel.macro';
+
+            class FooComponent extends Template {
+                @inject store;
+            }
+
+            const state = { [ ref( 'foo' ) ]: true };
+        `
+    } ]
+} );
---
+++ macros/ref.js
@@ -0,0 +1,76 @@
+const t = require( 'babel-types' );
+
+/**
+ * Macro for get/generate uniq reference
+ * @function ref
+ * @param {string} [name] Name of reference
+ *
+ * @example
+ * // Config in package.json
+ * // "babelMacros": {
+ * //   "ref": {
+ * //     "enabled": false
+ * //   }
+ * // }
+ *
+ * import { ref } from 'sham-ui-macro/babel.macro';
+ *
+ * const firstName = ref();
+ * const lastName = ref();
+ * const state = {
+ *     [ firstName ]: 'John',
+ *     [ lastName ]: 'Smith'
+ * }
+ * const fullName = state[ ref( 'firstName' ) ] + state[ ref( 'lastName' ) ];
+ *
+ * // ↓ ↓ ↓ ↓ ↓ ↓
+ *
+ * const firstName = 0;
+ * const lastName = 1;
+ * const state = {
+ *     [ firstName ]: 'John',
+ *     [ lastName ]: 'Smith'
+ * }
+ * const fullName = state[ 0 ] + state[ 1 ];
+ */
+
+/**
+ * @param {BabelPath} callExpressionPath
+ * @param {Object} config
+ * @param {Object} state
+ * @private
+ */
+module.exports = function ref( callExpressionPath, config, state ) {
+    const { uniq = false } = config;
+
+    let name;
+    const args = callExpressionPath.node.arguments;
+    if ( 0 === args.length  ) {
+        const parentPath = callExpressionPath.parentPath;
+        if ( !parentPath.isVariableDeclarator() ) {
+            throw new Error( 'Pass name to ref' );
+        }
+
+        // Use variable as name
+        name = parentPath.node.id.name;
+    } else {
+        name = args[ 0 ].value;
+    }
+
+    let identifier;
+    if ( uniq ) {
+        if ( !state.map.has( name ) ) {
+            state.map.set(
+                name,
+                state.map.size
+            );
+        }
+        identifier = t.NumericLiteral( state.map.get( name ) );
+    } else {
+        identifier = t.StringLiteral( name );
+    }
+
+    callExpressionPath.replaceWith(
+        identifier
+    );
+};
